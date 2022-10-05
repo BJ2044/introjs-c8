ded406b2bc72f156de243cfc5146d49423439f9a
Bradley Jones <bjones@Bradleys-MacBook-Air.local>
2022-10-05 17:06:37 -0400

    Day 3 Database

---
+++ students.js
@@ -0,0 +1,10 @@
+const students = require("./students.json")
+
+// display all the student's first names one at a time, one per line
+function showStudents(studentList) {
+   for (let i =0; i < studentList.length; i++) {
+    const student = studentList[i]
+    console.log(student.firstName)
+}
+}
+showStudents(students) 
